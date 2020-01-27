ea1187de40e8b1eab6e9684dfce74d892cb810f7
petergrajcar <peter.grajcar131@gmail.com>
2020-01-27 11:24:52 +0100

    Add log4j dependency

--- src/test/java/cz/cuni/mff/skychart/astronomy/CoordinatesTest.java
+++ src/test/java/cz/cuni/mff/skychart/astronomy/CoordinatesTest.java
@@ -1,5 +1,7 @@
 package cz.cuni.mff.skychart.astronomy;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
@@ -14,6 +16,8 @@ import static org.junit.Assert.*;
  */
 public class CoordinatesTest {
 
+    private static final Logger logger = LogManager.getLogger(CoordinatesTest.class);
+
     @Test
     public void localSiderealTimeCalculation() {
         ZonedDateTime time = LocalDateTime.parse("2019-01-01T08:00:00").atZone(ZoneOffset.UTC);
@@ -21,6 +25,7 @@ public class CoordinatesTest {
         Location location = new Location(48.2, 0.0);
 
         double lst = Coordinates.getLocalSiderealTime(time, location);
+        logger.debug("LST in hours: " + (lst / 3600d));
 
         assertEquals(52965.37916293584, lst, 1e-8);
     }
