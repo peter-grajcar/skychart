c7d7e35b2ac0be32e528838b31e3a17c04372a05
petergrajcar <peter.grajcar131@gmail.com>
2020-01-27 17:58:44 +0100

    Rewrite the BSC5 parsing in a more elegant way

--- src/main/java/cz/cuni/mff/skychart/catalogue/bsc5/BSC5Star.java
+++ src/main/java/cz/cuni/mff/skychart/catalogue/bsc5/BSC5Star.java
@@ -2,6 +2,7 @@ package cz.cuni.mff.skychart.catalogue.bsc5;
 
 import cz.cuni.mff.skychart.astronomy.EquatorialCoords;
 import cz.cuni.mff.skychart.catalogue.Star;
+import static cz.cuni.mff.skychart.catalogue.bsc5.BSC5Catalogue.BSC5Field;
 
 /**
  * An object representing star in BSC5 catalogue.
@@ -9,18 +10,6 @@ import cz.cuni.mff.skychart.catalogue.Star;
  * @author Peter Grajcar
  */
 public class BSC5Star extends Star {
-
-    /*
-     * indices of field separators
-     * TODO: make enum out of this
-     */
-    private static final int[] CATALOGUE_INDICES = {
-            0, 4, 14, 25, 31, 37, 41, 42, 43, 44, 49, 51, 60,
-            62, 64, 68, 69, 71, 73, 75, 77, 79, 83, 84, 86, 88, 90,
-            96, 102, 107, 108, 109, 114, 115, 120, 121, 126, 127,
-            147, 148, 154, 160, 161, 166, 170, 174, 176, 179, 180,
-            184, 190, 194, 196, 197
-    };
 
     /**
      * Returns star's name as combination of a greek or latin letter and constellation name (Bayer designation).
@@ -47,38 +36,27 @@ public class BSC5Star extends Star {
     /**
      * Parses the string argument as an entry in BSC5 catalogue.
      *
-     * @param s entry from BSC5 catalogue.
+     * @param entry entry from BSC5 catalogue.
      * @return a star matching the entry.
      * @throws BSC5FormatException if the string is not in valid format.
      */
-    public static BSC5Star parse(String s) throws BSC5FormatException {
-        if(s.length() < 197) {
-            StringBuilder sb = new StringBuilder(s);
-            for(int i = s.length(); i < 197; ++i)
+    public static BSC5Star parse(String entry) throws BSC5FormatException {
+        if(entry.length() < 197) {
+            StringBuilder sb = new StringBuilder(entry);
+            for(int i = entry.length(); i < 197; ++i)
                 sb.append(' ');
-            s = sb.toString();
+            entry = sb.toString();
         }
 
-        String[] fields = new String[CATALOGUE_INDICES.length - 1];
-        for(int i = 0; i < fields.length; ++i) {
-            fields[i] = s.substring(CATALOGUE_INDICES[i], CATALOGUE_INDICES[i + 1]);
-        }
-
-        String name = fields[1];
-        int dec_sign;
-        double ra_h, ra_m, ra_s, dec_d, dec_m, dec_s, visMag;
-        try {
-            ra_h = fields[19].isBlank() ? Double.NaN : Double.parseDouble(fields[19]);
-            ra_m = fields[20].isBlank() ? Double.NaN  : Double.parseDouble(fields[20]);
-            ra_s = fields[21].isBlank() ? Double.NaN  : Double.parseDouble(fields[21]);
-            dec_sign = "+".equals(fields[22]) ? 1 : -1;
-            dec_d = fields[23].isBlank() ? Double.NaN  : Double.parseDouble(fields[23]);
-            dec_m = fields[24].isBlank() ? Double.NaN  : Double.parseDouble(fields[24]);
-            dec_s = fields[25].isBlank() ? Double.NaN  : Double.parseDouble(fields[25]);
-            visMag = fields[28].isBlank() ? Double.NaN  : Double.parseDouble(fields[28]);
-        } catch (NumberFormatException e) {
-            throw new BSC5FormatException("Error while parsing BSC5 entry.", e);
-        }
+        String name = BSC5Field.NAME.asString(entry);
+        double ra_h = BSC5Field.RA_HOUR_J2000.asDouble(entry);
+        double ra_m = BSC5Field.RA_MIN_J2000.asDouble(entry);
+        double ra_s = BSC5Field.RA_SEC_J2000.asDouble(entry);
+        int dec_sign = BSC5Field.DEC_SIGN_J2000.asString(entry).equals("+") ? 1 : -1;
+        double dec_d = BSC5Field.DEC_DEG_J2000.asDouble(entry);
+        double dec_m = BSC5Field.DEC_MIN_J2000.asDouble(entry);
+        double dec_s = BSC5Field.DEC_SEC_J2000.asDouble(entry);
+        double visMag = BSC5Field.VISUAL_MAGNITUDE.asDouble(entry);
 
         double ra = ra_h + ra_m / 60d + ra_s / 3600d;
         double dec = dec_sign * (dec_d + dec_m / 60d + dec_s / 3600d);
